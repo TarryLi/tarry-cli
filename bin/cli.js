26b8c55bd207051e9fc9020989e433cede74338f
litaiyi <litaiyi@jd.com>
2023-07-20 17:13:25 +0800

    增加命令行输入信息

--- bin/cli.js
+++ bin/cli.js
@@ -14,26 +14,42 @@ program.version(`v${package.version}`);
 program.on('--help', () => { });
 
 program
-  .command('create')
-  .description('创建模版')
-  .action(async () => {
-    const { name } = await inquirer.prompt({
-      type: 'input',
-      name: 'name',
-      message: '请输入项目名称：'
-    })
-    console.log("项目名称：", name)
+  .command("create [projectName]")
+  .description("创建模版")
+  .option('-t, --template <template>', '模版名称')
+  .action(async (projectName, options) => {
 
-    const { template } = await inquirer.prompt({
-      type: 'list',
-      name: 'template',
-      message: '请选择模版：',
-      choices: templates // 模版列表
-    })
-    console.log('模版：', template)
+    // 1. 从模版列表中找到对应的模版
+    let project = templates.find(template => template.name === options.template)
+    // 2. 如果匹配到模版就赋值，没有匹配到就是undefined
+    let projectTemplate = project ? project.value : undefined
+    console.log('命令行参数：', projectName, projectTemplate) 
+
+    // 3. // 如果用户没有传入名称就交互式输入
+    if(!projectName) {
+      const { name } = await inquirer.prompt({
+        type: "input",
+        name: "name",
+        message: "请输入项目名称：",
+      })
+      projectName = name // 赋值输入的项目名称
+    }
+    console.log("项目名称：", projectName)
+
+    // 4. 如果用户没有传入模版就交互式输入
+    if(!projectTemplate) { 
+      const { template } = await inquirer.prompt({
+        type: 'list',
+        name: 'template',
+        message: '请选择模版：',
+        choices: templates // 模版列表
+      })
+      projectTemplate = template // 赋值选择的项目名称
+    }
+    console.log('模版：', projectTemplate)
 
     // 获取目标文件夹
-    const dest = path.join(process.cwd(), name)
+    const dest = path.join(process.cwd(), projectName)
     // 判断文件夹是否存在，存在就交互询问用户是否覆盖
     if (fs.existsSync(dest)) {
       const { force } = await inquirer.prompt({
@@ -45,18 +61,18 @@ program
       force ? fs.removeSync(dest) : process.exit(1)
     }
 
+    // 5. 开始下载模版
     // 定义loading
     const loading = ora('正在下载模版...')
     // 开始loading
     loading.start()
-    // 开心下载模版
-    downloadGitRepo(template, dest, (err) => {
+    downloadGitRepo(projectTemplate, dest, (err) => {
       if (err) {
         loading.fail('创建模版失败：' + err.message) // 失败loading
       } else {
         loading.succeed('创建模版成功!') // 成功loading
       }
     })
-  });
+  })
 
 program.parse(process.argv);
